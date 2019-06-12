e0e79d4682b8111886fa46b9647d93958ce65397
denis <vgdenisomar@gmail.com>
2019-06-11 18:35:32 -0600

    Creacion de la estructura de recetas y del metodo Get que muestra las recetas.

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,6 +2,10 @@ const express=require("express");
 const router=express.Router();
 
 
+const apiRecetas=require("./recetas");
+
+router.use("/recetas", apiRecetas);
+
 router.get("/",(req,res,next)=>{
     res.status(200).json({Mensaje:"Bienvenido al examen del primer parcial de SW"})
 })
---
+++ routes/api/recetas.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const router=express.Router();
+
+const uuid=require("uuid/v4");
+
+var coleccionRecetas=[];
+
+var estructuraRecetas= {
+    "id":"uuid",
+    "receta":"",
+    "precio":"",
+    "tipo":"",
+    "obervacion":"",
+    "estado":""
+};
+
+coleccionRecetas.push(
+    Object.assign(
+        estructuraRecetas,
+        {
+            "id":uuid(),
+            "receta":"1 panadol al dia",
+            "precio":"12",
+            "tipo":"1",
+            "obervacion":"sirve para el dolor de cabeza",
+            "estado":"ACT"
+        }
+
+
+    )
+);
+
+router.get("/",(req,res,next)=>{
+    res.status(200).json({Recetas:coleccionRecetas});
+})
+
+
+module.exports=router;
